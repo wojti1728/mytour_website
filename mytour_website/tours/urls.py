89d457a2eaf2ef4c02c8c0e14623cdd8c084c1b4
wojti1728 <wojti1728@gmail.com>
2023-10-18 20:26:28 +0200

    more initally changes

--- mytour_website/tours/urls.py
+++ mytour_website/tours/urls.py
@@ -1,6 +1,12 @@
 from . import views
 from django.urls import path, include
 
+
 urlpatterns = [
+    # Path converters
+    # int: numbers
+    # str: strings
+    # path: whole urls
     path('', views.home, name="home"),
+    path('<int:year>/<str:month>/', views.home, name="home"),
 ]
--- mytour_website/tours/views.py
+++ mytour_website/tours/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 
-def home(request):
-    return render(request, 'home.html', {})
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    # convert month from name to number
+    month = month.capitalize()
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    now = datetime.now()
+    current_year = now.year
+
+    time = now.strftime('%I:%M %p')
+    # create a calendar
+    cal = HTMLCalendar().formatmonth(
+        year,
+        month_number
+    )
+
+    return render(request, 'events/home.html', {
+        "year": year,
+        "month": month,
+        "month_number": month_number,
+        "cal": cal,
+        "current_year": current_year,
+        "now": now,
+        "time": time,
+    })
