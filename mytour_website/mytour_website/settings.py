af31bd54441788958a152392ee8f5ffa987eed76
wojti1728 <wojti1728@gmail.com>
2023-10-18 21:57:19 +0200

    templates plus databases

--- mytour_website/mytour_website/settings.py
+++ mytour_website/mytour_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +77,15 @@ WSGI_APPLICATION = 'mytour_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mytours',
+        'USER': 'postgres',
+        'PASSWORD': 'student',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -106,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
@@ -119,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- mytour_website/tours/models.py
+++ mytour_website/tours/models.py
@@ -1,3 +1,82 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField('PLace Name', max_length=100)
+    description = models.TextField(max_length=300)
+    address = models.CharField(max_length=200)
+    web = models.URLField('Web Address')
+
+    def __str__(self):
+        return f"This is a {self.name}"
+
+
+class Transport(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    type = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"Transport: {self.name}"
+
+
+class Accommodation(models.Model):
+    name = models.CharField('Accomadation Name', max_length=100)
+    description = models.TextField(max_length=300)
+    address = models.CharField(max_length=200)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"Accommodation: {self.name}"
+
+
+class Sightseeing(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    address = models.ForeignKey(Place, on_delete=models.CASCADE)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"Sightseeing: {self.name}"
+
+
+class Day(models.Model):
+    date = models.DateField()
+    locations = models.ManyToManyField(Location, related_name='days')
+    transports = models.ManyToManyField(Transport, related_name='days')
+    accomadation = models.ForeignKey(
+        Accommodation, on_delete=models.CASCADE, related_name='days')
+    food_plans = models.ManyToManyField(FoodPlan, related_name='days')
+    sightseeing = models.ManyToManyField(Sightseeing, related_name='days')
+
+    def __str__(self):
+        return f"Day: {self.date}"
+
+
+class ThingsList(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+
+    def __str__(self):
+        return f"ThingsList: {self.title}"
+
+
+class Tour(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=300)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    price = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    day = models.ManyToManyField(
+        Day, related_name='tours')
+    things_list = models.ManyToManyField(
+        ThingsList, related_name='tours', default=None)
+
+    def __str__(self):
+        return f"Tour: {self.title}"
+
+    def get_absolute_url(self):
+        """Returns the URL to access a detail record for this book."""
+        return reverse('tour-detail', args=[str(self.id)])
