21c6d4924ec5be686cec79a0c91d9fa77920c99d
wojti1728 <wojti1728@gmail.com>
2023-10-22 21:42:15 +0200

    authentication

--- mytour_website/users/views.py
+++ mytour_website/users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
+from users.forms import RegisterUserForm
 
 
 def login_user(request):
@@ -28,3 +31,21 @@ def logout_user(request):
         request, ("You Were Loged Out"))
 
     return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(
+                request, ("Registration Succesfully!"))
+            return redirect('home')
+    else:
+        form = RegisterUserForm()
+
+    return render(request, 'authenticate/register_user.html', {'form': form})
